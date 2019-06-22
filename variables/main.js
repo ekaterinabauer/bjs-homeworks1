26f19df827e46d4573e516162c4ba84a96c1000d
Ekaterina Bauer <bauer-katerina@mail.ru>
2019-06-22 17:35:25 +0300

    Выполенное задание

---
+++ variables/main.js
@@ -0,0 +1,18 @@
+// Задача 1
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+let averageRating = (algebra + geography + physics) / 3;
+console.log(averageRating);
+
+// Задача 2
+let name = 'Ekaterina';
+let message = `Привет, мир! Меня зовут ${name}`;
+console.log(message);
+
+// Задача 3
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
